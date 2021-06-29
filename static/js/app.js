52639e7a1bd6c2aadc0ddd4f22f57e716d65b317
Alivia Varvel <atvarvel@gmail.com>
2021-06-29 19:15:52 -0400

    added comments

--- static/js/app.js
+++ static/js/app.js
@@ -17,31 +17,34 @@ function sampleMetadata(sample) {
     });
 }
 
+// Create Bar and Bubble plots
 function createPlots(sample) {
     d3.json("../samples.json").then((data) => {
         var samples = data.samples;
+        // Filter data based on current sample
         var filteredData = samples.filter(sampleObject => sampleObject.id == sample);
         var sampleData = filteredData[0];
-
+        // Create variables for ids, labels, and values
         var ids = sampleData.otu_ids;
         var labels = sampleData.otu_labels;
         var values = sampleData.sample_values;
-
+        // Get data for bar chart
         var barData = [{
             y: ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
             x: values.slice(0, 10).reverse(),
             text: labels.slice(0, 10).reverse(),
             type: "bar",
+            // Make the bars horizontal
             orientation: "h"
         }];
-
+        // Create title and margins for chart
         var barLayout = {
             title: "Top 10 Bacteria Cultures Found",
             margin: { t: 30, l: 150 }
         };
-        
+        // Plot bar chart
         Plotly.newPlot("bar", barData, barLayout);
-
+        // Get data for bubble chart
         var bubbleData = [{
             x: ids,
             y: values,
@@ -52,18 +55,18 @@ function createPlots(sample) {
                 size: values
             }
         }];
-
+        // Create title, margins, and axis labels for chart
         var bubbleLayout = {
             title: "Bacteria Cultures Per Sample",
             margin: { t: 30 },
             xaxis: { title: "OTU ID" },
             hovermode: "closest"
         };
-
+        // Plot bubble chart
         Plotly.newPlot("bubble", bubbleData, bubbleLayout);
     });
 }
-
+// Initialize the charts when page is opened
 function init() {
     var selector = d3.select("#selDataset");
 
@@ -75,17 +78,17 @@ function init() {
                 .text(sample)
                 .property("value", sample);
         });
-
+        // Plot charts using the first sample
         var firstSample = names[0];
         createPlots(firstSample);
         sampleMetadata(firstSample);
     });
 }
-
+// Initialize
 init();
 
 var selector = d3.select("#selDataset");
-
+// Update charts when a new sample is selected from the dropdown
 selector.on("change", function() {
     var selectedValue = selector.property("value");
     createPlots(selectedValue);
